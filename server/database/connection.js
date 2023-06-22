eaab86b4cb635d682bca15b43ec384e0335cd817
ShahbazKapoor <123551531+ShahbazKapoor@users.noreply.github.com>
2023-06-22 15:52:20 +0530

    Update connection.js

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql');
 
 const connectDB = mysql.createConnection({
-    host: 'localhost',
+    host: 'bryasxyvusuqimtapky8-mysql.services.clever-cloud.com',
     port: 3306,
-    database: 'mycrud',
-    user: 'root',
-    password: 'root123',
-    multipleStatements: true
+    user: 'uubwjacmgltjdy9a',
+    password: 'wjKGKMxO8uxZCx3NAJna',
+    database: 'bryasxyvusuqimtapky8',
+    connectTimeout: 20000, // Increased connection timeout to 20 seconds
 });
 
 module.exports = connectDB;
